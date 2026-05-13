a1e9173ebade6f646ce3c1e4371e1718ae1923e0
cdCharly <charly44310@gmail.com>
2026-05-13 22:21:00 +0200

    j'ai modifié les parametres des requetes et des exec

--- server.cpp
+++ server.cpp
@@ -5,13 +5,10 @@
 #include <sqlite3.h>
 
 
-
-
-
 int main() {
     sqlite3* db;
-    char** errMsgUserDb = nullptr;
-    char** errMsgMessagesDb = nullptr;
+    char* errMsgUserDb = nullptr;
+    char* errMsgMessagesDb = nullptr;
 
     // ouvrir/créer le fichier db
     int initdb = sqlite3_open("social.db", &db);
@@ -27,13 +24,22 @@ int main() {
 
 
     // activer les cles etrangeres
-    sqlite3_exec(db, "PRAGMA foreign_keys = ON", nullptr, nullptr, errMsgMessagesDb);
+    sqlite3_exec(db, "PRAGMA foreign_keys = ON", nullptr, nullptr, &errMsgMessagesDb);
 
     // créer les tables des bases
-    sqlite3_exec(db,"CREATE TABLE User (isUser INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, lastName TEXT, password TEXT)",
-        nullptr, nullptr, errMsgUserDb);
-    sqlite3_exec(db,"CREATE TABLE Messages (idMessage INTEGER PRIMARY KEY AUTOINCREMENT, contenu TEXT, date DATETIME CURRENT_TIMESTAMP, userID INTEGER, FOREIGN KEY(userID) REFERENCES User(id)",
-        nullptr, nullptr, errMsgMessagesDb);
+    sqlite3_exec(db,"CREATE TABLE User (idUser INTEGER PRIMARY KEY AUTOINCREMENT, "
+                    "name TEXT, "
+                    "lastName TEXT, "
+                    "password TEXT)",
+        nullptr, nullptr, &errMsgUserDb);
+
+
+    sqlite3_exec(db,"CREATE TABLE Messages (idMessage INTEGER PRIMARY KEY AUTOINCREMENT, "
+                    "contenu TEXT, "
+                    "date DATETIME DEFAULT CURRENT_TIMESTAMP, "
+                    "userID INTEGER, "
+                    "FOREIGN KEY(userID) REFERENCES User(id)",
+        nullptr, nullptr, &errMsgMessagesDb);
 
 
 
