b565bc9724364177184833bf51829fc2c3d7acca
CATIN--RICO Charly <charly.catin-rico@etu.univ-nantes.fr>
2026-05-13 22:43:10 +0200

    ajout de fonction pour inserer dans user et pour lire une table aide gemini pour le callback j'ai testé ca fonctionne bien

--- server.cpp
+++ server.cpp
@@ -3,7 +3,74 @@
 //
 #include <iostream>
 #include <sqlite3.h>
+using namespace std;
 
+
+static int callback_affichage(void* data, int argc, char** argv, char** azColName) {
+    for (int i = 0; i < argc; i++) {
+        // On affiche: NomDeLaColonne = Valeur
+        cout << azColName[i] << " = " << (argv[i] ? argv[i] : "NULL") << " | ";
+    }
+    cout << endl; // Saut de ligne pour le prochain utilisateur
+    return 0;
+}
+
+
+
+// inserer dans une table
+void insertIntoUserTable(sqlite3 *db){
+
+    // variables
+    string requete, name, lastname, password = "";
+    char* errMsg = nullptr;
+
+    // reception infos
+    cout << "Ecrire user name" << endl;
+    cin >>name;
+    cout << "Ecrire lastname" << endl;
+    cin >>lastname;
+    cout << "Ecrire password" << endl;
+    cin >>password;
+
+    // creation requete + execution
+    requete = "INSERT INTO User (name, lastName, password) "
+              "VALUES ('" + name + "', '" + lastname + "', '" + password + "');";
+
+    sqlite3_exec(db, requete.c_str(), nullptr, nullptr, &errMsg);
+
+    // conclusion
+    if (errMsg != nullptr) {
+        cout << "probleme insertion" << errMsg << endl;
+    }
+    else {
+        cout << "insertion finie" << endl;
+    }
+
+    // delete le ptr pour vider la memoire
+    sqlite3_free(errMsg);
+}
+
+
+void selectAllTable(sqlite3* db, const string tableName) {
+
+    // variables
+    char* errMsg = nullptr;
+
+    // construction requete + execution
+    string requete = "SELECT * FROM "+tableName;
+    sqlite3_exec(db, requete.c_str(), callback_affichage, nullptr, &errMsg);
+
+    // conclusion
+    if (errMsg != nullptr) {
+        cout << "probleme lecture" << errMsg << endl;
+    }
+    else {
+        cout << "lecture finie" << endl;
+    }
+
+    // delete le ptr pour vider la memoire
+    sqlite3_free(errMsg);
+}
 
 int main() {
     sqlite3* db;
@@ -16,7 +83,7 @@ int main() {
     if (initdb != SQLITE_OK) {
         // Err
         std::cerr << "Erreur à l'ouverture de la db : " << sqlite3_errmsg(db) << std::endl;
-        return 1;
+
     } else {
         // ok
         std::cout << "Base de données ouverte avec succès !" << std::endl;
@@ -40,6 +107,24 @@ int main() {
                     "userID INTEGER, "
                     "FOREIGN KEY(userID) REFERENCES User(id)",
         nullptr, nullptr, &errMsgMessagesDb);
+
+
+    // insertion d'un valeur
+    /*
+    insertIntoUserTable(db);
+    insertIntoUserTable(db);
+    insertIntoUserTable(db);
+    selectAllTable(db, "User");
+    */
+
+
+
+
+
+
+
+
+
 
 
 
