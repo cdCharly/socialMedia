bd213a70dc89d94cc57b9dca3221d1d25971a262
cdCharly <charly44310@gmail.com>
2026-05-19 21:52:21 +0200

    ajout login + register + repasse pour la lecture des messages

--- server.cpp
+++ server.cpp
@@ -8,8 +8,10 @@
 #include <sqlite3.h>
 #include <cstring>
 #include <thread>
-#include <cstdlib> //pour utiliser atoi()
+#include <mutex>    // pour la file d'attente si deux connection au mee moment
+//#include <cstdlib> //pour utiliser atoi()
 using namespace std;
+std::mutex dbMutex;
 
 
 static int callback_affichage(void* data, int argc, char** argv, char** azColName) {
@@ -24,10 +26,10 @@ static int callback_affichage(void* data, int argc, char** argv, char** azColNam
 
 static int callback_affichageClient(void* data, int argc, char** argv, char** azColName) {
     // 1. On récupère le socket client
-    int clientSock = *(int*)data;
+    int clientSock = *static_cast<int *>(data);
 
     // 2. On prépare une ligne de texte pour ce message
-    string reponse = "";
+    string reponse;
     for (int i = 0; i < argc; i++) {
         reponse += azColName[i];
         reponse += ": ";
@@ -49,7 +51,7 @@ static int callback_login(void* data, int argc, char** argv, char** azColName) {
     if (argc > 0 && argv[0] != nullptr) {
 
         // 1. On transforme le void* en un pointeur d'entier (int*)
-        int* pointeurId = (int*)data;
+        int* pointeurId = static_cast<int *>(data);
 
         // 2. On convertit le texte renvoyé par SQLite (argv[0]) en vrai entier (int)
         // et on le stocke à l'adresse mémoire de notre pointeur
@@ -64,7 +66,7 @@ static int callback_login(void* data, int argc, char** argv, char** azColName) {
 void insertIntoUserTable(sqlite3 *db){
 
     // variables
-    string requete, name, lastname, password = "";
+    string requete, name, lastname, password;
     char* errMsg = nullptr;
 
     // reception infos
@@ -79,7 +81,11 @@ void insertIntoUserTable(sqlite3 *db){
     requete = "INSERT INTO User (name, lastName, password) "
               "VALUES ('" + name + "', '" + lastname + "', '" + password + "');";
 
-    sqlite3_exec(db, requete.c_str(), nullptr, nullptr, &errMsg);
+    {
+        std::lock_guard<std::mutex> lock(dbMutex);
+        sqlite3_exec(db, requete.c_str(), nullptr, nullptr, &errMsg);
+    }
+
 
     // conclusion
     if (errMsg != nullptr) {
@@ -99,7 +105,7 @@ void insertIntoUserTable(sqlite3 *db){
 void insertIntoMessagesTable(sqlite3 *db) {
 
     // variables
-    string requete, contenu, idUser = "";
+    string requete, contenu, idUser;
     char*errMsg = nullptr;
 
     // reception infos
@@ -110,7 +116,12 @@ void insertIntoMessagesTable(sqlite3 *db) {
 
     // construction requete
     requete = "INSERT INTO Messages (contenu, userID) VALUES ('" + contenu +"', '" + idUser + "');";
-    sqlite3_exec(db, requete.c_str(), callback_affichage, nullptr, &errMsg);
+
+    {
+        std::lock_guard<std::mutex> lock(dbMutex);
+        sqlite3_exec(db, requete.c_str(), callback_affichage, nullptr, &errMsg);
+    }
+
 
     // conclusion
     if (errMsg != nullptr) {
@@ -125,14 +136,19 @@ void insertIntoMessagesTable(sqlite3 *db) {
 }
 
 
-void selectAllTable(sqlite3* db, const string tableName) {
+void selectAllTable(sqlite3* db, const string &tableName) {
 
     // variables
     char* errMsg = nullptr;
 
     // construction requete + execution
     string requete = "SELECT * FROM "+tableName;
-    sqlite3_exec(db, requete.c_str(), callback_affichage, nullptr, &errMsg);
+
+    {
+        std::lock_guard<std::mutex> lock(dbMutex);
+        sqlite3_exec(db, requete.c_str(), callback_affichage, nullptr, &errMsg);
+    }
+
 
     // conclusion
     if (errMsg != nullptr) {
@@ -158,10 +174,17 @@ string parseStr(int indStart, int indEnd,char* msg) {
 // m.substr(indStart, m.length() - indStart - 1)
 
 // inserer un message dans Messages en parametres
-void insertIntoMessagesTable(sqlite3* db, string msg) {
+void insertIntoMessagesTable(sqlite3 *db, const string &msg, const int idUser) {
+
     char* errMsg = nullptr;
-    string requete = "INSERT INTO Messages (contenu) VALUES ('"+msg+"');";
-    sqlite3_exec(db, requete.c_str(), nullptr, nullptr, &errMsg);
+    string id = to_string(idUser);
+    string requete = "INSERT INTO Messages (contenu, userID) VALUES ('"+msg+"', '" + id + "');";
+
+    {
+        std::lock_guard<std::mutex> lock(dbMutex);
+        sqlite3_exec(db, requete.c_str(), nullptr, nullptr, &errMsg);
+    }
+
 
     if (errMsg != nullptr) {
         cout << "probleme insertion messages " << errMsg << endl;
@@ -184,33 +207,58 @@ void gererClient(int clientSocket, sqlite3* db) {
 
     while (true) {
         // on recupere le message via un buffer
-        char buffer[1024]; // 1024 octets de place pour ecrire
-        memset(buffer, 0, sizeof(buffer));
-        int octRecus = recv(clientSocket, buffer, sizeof(buffer), 0);
+        char buffer[1024] = {}; // 1024 octets de place pour ecrire
+        size_t octRecus = recv(clientSocket, buffer, sizeof(buffer), 0);
 
-        // verif si le message est LIST alors on donne la liste de tout les messages
-        const char* lister = "LIST";        // commande client pour avoir la liste des messages
-        const char* ecrire = "MSG:";        // commande client pour ecrire un message
-        const char* login = "LOGIN:";
+
+
+
+        /*
+        const char* instructions = "REG:name:lastname:password | LST: | MSG:contenu | LOG:name:password \n";
+        send(clientSocket, instructions, strlen(instructions), 0);
+        */
 
         if (octRecus > 0) {
 
+            // verif si le message est LIST alors on donne la liste de tout les messages
+            const char* lister = "LST:";        // commande client pour avoir la liste des messages
+            const char* ecrire = "MSG:";        // commande client pour ecrire un message
+            const char* login = "LOG:";       // commande pour se log
+            const char* reg = "REG:";           // commande pour register
+
             // recuperer la liste des messages
             if (strncmp(buffer, lister, 4) == 0) {
+                if (idUser != -1) {
 
-                sqlite3_exec(db, "SELECT contenu, date FROM Messages;", callback_affichageClient, &clientSocket, &errMsgMessagesDb); // mettre le resultat dans le socket client avec le callback
-                string fin = "---- fin des messages -----\n";
-                send(clientSocket, fin.c_str(), strlen(fin.c_str()), 0);        // renvoyer le message de fin
+                    {
+                        std::lock_guard<std::mutex> lock(dbMutex);
+                        //sqlite3_exec(db, "SELECT contenu FROM Messages ;", callback_affichageClient, &clientSocket, &errMsgMessagesDb);
+                        sqlite3_exec(db, "SELECT User.name, Messages.contenu FROM Messages LEFT JOIN User ON User.idUser = Messages.userID;", callback_affichageClient, &clientSocket, &errMsgMessagesDb);
+                    }
+
+                    string fin = "---- fin des messages -----\n";
+                    send(clientSocket, fin.c_str(), strlen(fin.c_str()), 0);        // renvoyer le message de fin
+                }
+                else {
+                    send(clientSocket, "err log \n", strlen("err log \n"), 0);
+
+                }
             }
 
             // envoyer un message dans la db
-            if (strncmp(buffer, ecrire, 4) == 0) {
-                string m = reinterpret_cast<const char*>(buffer);
-                string messageClient = parseStr(4, m.length() - 1 , buffer);
-                insertIntoMessagesTable(db, messageClient);     // on met le message dans la db
+            else if (strncmp(buffer, ecrire, 4) == 0) {
+                if (idUser != -1) {
+                    string m = reinterpret_cast<const char*>(buffer);
+                    const int len = static_cast<int>(m.length());
+                    string messageClient = parseStr(4, len - 1 , buffer);
+                    insertIntoMessagesTable(db, messageClient, idUser);     // on met le message dans la db
+                }
+                else {
+                    send(clientSocket, "err log \n", strlen("err log \n"), 0);
+                }
             }
 
-            if (strncmp(buffer, login, 4) == 0) {
+            else if (strncmp(buffer, login, 4) == 0) {
                 string m = reinterpret_cast<const char*>(buffer);
 
                 size_t premierSlice = m.find(':');
@@ -219,21 +267,34 @@ void gererClient(int clientSocket, sqlite3* db) {
                 if (deuxiemeSlice != std::string::npos) {
 
                     // recuperer nom
-                    int indDebutNom = premierSlice + 1;
-                    int longeurNom = deuxiemeSlice - indDebutNom;
+                    size_t indDebutNom = premierSlice + 1;
+                    size_t longeurNom = deuxiemeSlice - indDebutNom;
                     string nom = m.substr(indDebutNom, longeurNom);
 
                     // recuperer mdp
-                    int indDebutMdp = deuxiemeSlice + 1;
-                    int longeurMdp = m.length() - indDebutMdp - 1;
+                    size_t indDebutMdp = deuxiemeSlice + 1;
+                    size_t longeurMdp = m.length() - indDebutMdp - 1;
                     string mdp = m.substr(indDebutMdp, longeurMdp);
 
-                    cout << "login " << nom << " / " << mdp << endl;
+                    cout << "login \n" << nom << " / " << mdp << endl;
 
 
+                    idUser = -1;
                     // partie verif db
                     string requete = "SELECT idUser FROM User WHERE name = '" + nom + "' AND PASSWORD = '" + mdp + "' ;";
-                    sqlite3_exec(db, requete.c_str(), callback_login, &idUser, &errMsgUser);
+                    {
+                        std::lock_guard<std::mutex> lock(dbMutex);
+                        sqlite3_exec(db, requete.c_str(), callback_login, &idUser, &errMsgUser);
+                    }
+
+                    if (idUser != -1) {
+                        string ok = "Connexion reussie ! Ton ID est " + to_string(idUser) + "\n";
+                        send(clientSocket, ok.c_str(), ok.length(), 0);
+                    } else {
+                        string ko = "Erreur: Nom ou mot de passe incorrect.\n";
+                        send(clientSocket, ko.c_str(), ko.length(), 0);
+                    }
+
 
 
                 }
@@ -242,13 +303,67 @@ void gererClient(int clientSocket, sqlite3* db) {
                     string erreurFormat = "Erreur: Format attendu LOGIN:nom:password\n";
                     send(clientSocket, erreurFormat.c_str(), erreurFormat.length(), 0);
                 }
+            }
 
 
+            else if (strncmp(buffer, reg, 4) == 0) {
+                string m(buffer);
+                // separateurs
+                size_t p1 = m.find(':');
+                size_t p2 = m.find(':', p1 + 1);
+                size_t p3 = m.find(':', p2 + 1);
+
+                if (p2 != std::string::npos && p3 != std::string::npos) {
+
+                    // prenom
+                    size_t debPre = p1 + 1;
+                    size_t longPre = p2 - debPre;
+                    string pre = m.substr(debPre, longPre);
+
+                    // nom de famille
+                    size_t debNom = p2 + 1;
+                    size_t longNom = p3 - debNom;
+                    string nom = m.substr(debNom, longNom);
+
+                    // password
+                    size_t debMdp = p3 + 1;
+                    size_t longMdp = m.length() - debMdp - 1;
+                    string mdp = m.substr(debMdp, longMdp);
+
+                    cout << "new register \n" << pre << nom << endl;
+
+                    // DB request
+                    char* errREG = nullptr;
+                    string requete = "INSERT INTO User (name, lastName, password) VALUES ('"+ nom + "', '" + pre + "', '" + mdp + "')";
+                    {
+                        std::lock_guard<std::mutex> lock(dbMutex);
+                        sqlite3_exec(db, requete.c_str(), nullptr, nullptr, &errREG);
+                    }
+
+                    // err d'insertion
+                    if (errREG != nullptr) {
+                        string e = errREG;
+                        e.append("\n");
+                        cout << "probleme insertion messages \n" << errREG << endl;
+                        send(clientSocket, errREG, e.length(), 0);
+                        sqlite3_free(errREG);
+                    }
+                    else {
+                        cout << "register ok" << endl;
+                        string valid = "inscription valide\n";
+                        send(clientSocket, valid.c_str(), valid.length(), 0);
+                    }
+                }
+                else {
+                    string err = "erreur de format\n";
+                    send(clientSocket, err.c_str(), err.length(), 0);
+                }
             }
         }
 
         else {
-            cout << "ecriture probleme" << endl;
+            cout << "client deconnecte" << endl;
+            break; // sortie du client
         }
     }
     close(clientSocket);
@@ -362,5 +477,5 @@ int main() {
 
         }
 
-    return 0;
+    // return 0;
     }
